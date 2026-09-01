6027ca01c255a50a0efd5438b76842f2acc355f4
Ghennadi Coca <ghennadicoca@Ghennadis-MacBook-Pro.local>
2026-09-01 14:44:08 +0200

    Initial commit

---
+++ Lesson_01/type_strings.py
@@ -0,0 +1,13 @@
+age = 18 # int
+price = 19.99 # float
+name = "Sveta" # str
+is_name = True # bool
+
+fruits = ["apple", "orange"] # list
+coordinate = [1, 2, 3] # tuple
+student ={"name": "Sveta", "age": 18} # dictionar
+unique_numbers = {1,2,9,6,9,2,0,1}
+print(unique_numbers)
+print(type(age))
+print(type(price))
+print(type(name))
